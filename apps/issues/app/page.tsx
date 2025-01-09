24fcc12f69fe6936e788d6f27c92c5e8ab3c0a33
seob717 <dev.seob717@gmail.com>
2025-01-09 18:23:01 +0900

    fix : issues 복제

--- apps/issues/app/page.tsx
+++ apps/issues/app/page.tsx
@@ -1,14 +1,5 @@
-import styles from "./page.module.css";
-import {testParams} from "@/app/constants/test";
-import Link from "next/link";
+const Page = () => {
+  return <div>hello world</div>;
+};
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <span>build test</span>
-      {testParams.map(({id,title})=>{
-        return <Link href={`/${id}/${title}`}>{title}</Link>
-      })}
-    </div>
-  );
-}
+export default Page;
