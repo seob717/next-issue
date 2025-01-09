c5cb33ea5a6ab4b31b6dd27bd361c99eed98b538
seob717 <dev.seob717@gmail.com>
2025-01-09 18:13:19 +0900

    fix : 테스트 페이지 오류 수정

--- apps/issues/app/page.tsx
+++ apps/issues/app/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className={styles.page}>
+      <span>build test</span>
       {testParams.map(({id,title})=>{
         return <Link href={`/${id}/${title}`}>{title}</Link>
       })}
